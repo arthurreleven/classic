20ba53ee2d98bb8ba1dae6f6c90b6545eef45315
Arthur <163530218+arthurreleven@users.noreply.github.com>
2026-02-03 20:59:25 -0300

    Delete backend directory

--- backend/src/routes/jokenpo.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import JokenpoController from '../controllers/jokenpo.controller';
-
-const router = Router();
-
-// Define a rota POST /play
-router.post('/play', JokenpoController.play);
-
-export default router;
